e248a7c8492a9bb3642a25b019422e482728b47f
Cristiano Oliveira <cristiano.bento@kroton.com.br>
2019-06-11 08:39:23 -0300

    feat: adding first reducers and actions

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import { getnitialDataI, getInitialData } from "../utils/api";
+import { receiveUsers } from "../actions/users";
+import { receiveTweets } from "../actions/tweets";
+import { setAuthedUser } from "../actions/authedUser";
+
+const AUTHED_ID = "tylermcginnis";
+
+export function handleInitialData() {
+  return dispatch => {
+    return getInitialData().then(({ tweets, users }) => {
+      dispatch(receiveUsers(users));
+      dispatch(receiveTweets(tweets));
+      dispatch(setAuthedUser(AUTHED_ID));
+    });
+  };
+}
